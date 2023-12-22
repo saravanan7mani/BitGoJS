9d621e75632b1bc3e65605dce550896ed6169c84
AJ Srinivas <ajsrinivas@bitgo.com>
2023-12-22 18:08:40 +0530

    feat(sdk-coin-zketh): add zketh token support
    Ticket: WIN-1422

---
+++ modules/sdk-coin-zketh/src/zkethToken.ts
@@ -0,0 +1,30 @@
+/**
+ * @prettier
+ */
+import { EthLikeTokenConfig } from '@bitgo/statics';
+import { BitGoBase, CoinConstructor, NamedCoinConstructor } from '@bitgo/sdk-core';
+import { CoinNames, EthLikeToken } from '@bitgo/abstract-eth';
+
+export { EthLikeTokenConfig };
+
+export class ZkethToken extends EthLikeToken {
+  public readonly tokenConfig: EthLikeTokenConfig;
+  static coinNames: CoinNames = {
+    Mainnet: 'zketh',
+    Testnet: 'tzketh',
+  };
+  constructor(bitgo: BitGoBase, tokenConfig: EthLikeTokenConfig) {
+    super(bitgo, tokenConfig, ZkethToken.coinNames);
+  }
+  static createTokenConstructor(config: EthLikeTokenConfig): CoinConstructor {
+    return super.createTokenConstructor(config, ZkethToken.coinNames);
+  }
+
+  static createTokenConstructors(): NamedCoinConstructor[] {
+    return super.createTokenConstructors(ZkethToken.coinNames);
+  }
+
+  getFullName(): string {
+    return 'Zketh Token';
+  }
+}
